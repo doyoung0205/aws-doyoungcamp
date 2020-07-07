01688005e9eefae14ee8938212595fef6f887c5e
doyoung0205 <doyoung0205@naver.com>
2020-07-08 00:21:41 +0900

    INIT

--- src/main/java/com/doyoungcamp/aws/springbootapp/hello/ui/HelloController.java
+++ src/main/java/com/doyoungcamp/aws/springbootapp/hello/ui/HelloController.java
@@ -1,13 +1,16 @@
 package com.doyoungcamp.aws.springbootapp.hello.ui;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
-    @GetMapping("/")
-    public String indexPage() {
+    @GetMapping({"/", "hello"})
+    public String helloWorld(@RequestParam(required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
         return "index";
     }
 }
