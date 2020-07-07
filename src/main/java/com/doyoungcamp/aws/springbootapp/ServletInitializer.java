89b1886a4dc8ff88cd29717474fa99ff0c9397eb
doyoung0205 <doyoung0205@naver.com>
2020-07-08 00:24:35 +0900

    initialize

--- src/main/java/com/doyoungcamp/aws/springbootapp/ServletInitializer.java
+++ src/main/java/com/doyoungcamp/aws/springbootapp/ServletInitializer.java
@@ -1,14 +1,15 @@
-package com.doyoungcamp.aws.springbootapp;
-
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-public class ServletInitializer extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SpringbootAppApplication.class);
-	}
-
-}
-
+//package com.doyoungcamp.aws.springbootapp;
+//
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//
+//
+//public class ServletInitializer extends SpringBootServletInitializer {
+//
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(SpringbootAppApplication.class);
+//	}
+//
+//}
+//
